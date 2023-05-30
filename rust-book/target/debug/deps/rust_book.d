395e846d5fbfea151b8f13b6fb2f63f98d08a5c5
xuyanshi <xuyanshi@ruc.edu.cn>
2023-05-30 20:48:26 +0800

    delete the project rust-book

--- rust-book/target/debug/deps/rust_book.d
+++
@@ -1,5 +0,0 @@
-C:\Users\xuyanshi\Code\Self-Learning\TP201\rust-book\target\debug\deps\rust_book.exe: src\main.rs
-
-C:\Users\xuyanshi\Code\Self-Learning\TP201\rust-book\target\debug\deps\rust_book.d: src\main.rs
-
-src\main.rs:
--- rust-book/target/debug/rust-book.d
+++
@@ -1,1 +0,0 @@
-C:\Users\xuyanshi\Code\Self-Learning\TP201\rust-book\target\debug\rust-book.exe: C:\Users\xuyanshi\Code\Self-Learning\TP201\rust-book\src\main.rs
