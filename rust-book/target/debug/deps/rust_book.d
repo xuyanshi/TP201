045495e5a51696803a1599c02f2b6e389ba8433e
xuyanshi <xuyanshi@ruc.edu.cn>
2023-05-30 20:40:53 +0800

    update

---
+++ rust-book/target/debug/deps/rust_book.d
@@ -0,0 +1,5 @@
+C:\Users\xuyanshi\Code\Self-Learning\TP201\rust-book\target\debug\deps\rust_book.exe: src\main.rs
+
+C:\Users\xuyanshi\Code\Self-Learning\TP201\rust-book\target\debug\deps\rust_book.d: src\main.rs
+
+src\main.rs:
---
+++ rust-book/target/debug/rust-book.d
@@ -0,0 +1,1 @@
+C:\Users\xuyanshi\Code\Self-Learning\TP201\rust-book\target\debug\rust-book.exe: C:\Users\xuyanshi\Code\Self-Learning\TP201\rust-book\src\main.rs
